2f231aa344cef4726b68236da60617f3d16cba99
b1mind <brent@1mindsolutions.com>
2021-01-27 22:33:02 -0700

    🤩 layout half way

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,81 @@
 <script>
   import Card from './components/Card.svelte'
-
-  const user = 'World'
-  let count = 0
 </script>
 
-<header>
-  <h1>This is Svelte-Pack!</h1>
-  <p>Y0 {user}</p>
-
-  <button on:click={() => count++}>
-    Clicked
-    {count}
-    {count === 1 ? 'time' : 'times'}
-  </button>
-</header>
-
 <main>
-  <Card>make me a phone</Card>
+  <div class="hero">
+    <Card />
+    <div class="hero-text">
+      <h1>Book with Us</h1>
+      <p>
+        Stay in touch with this amazing Svelte app by booking a appointment. Play around
+        as there maybe many fun working features to this challenge by Kevin Powell based
+        on Frontend Mentors
+      </p>
+    </div>
+  </div>
 </main>
 
 <style lang="scss">
   //< style more
 
-  $max: 1200px;
-
-  header {
-    max-width: $max;
-    margin: 1rem auto;
-    padding: 0.5rem 1rem;
-  }
+  $max: 1900px;
+  $min: 360px;
 
   main {
+    position: relative;
+    margin: 0 auto;
     max-width: $max;
-    margin: 10px auto;
+    min-height: 920px;
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 360px));
-    gap: 3rem;
     place-content: center;
+    background-color: var(--clr-gray);
+    overflow: hidden;
+    z-index: 0;
+
+    &::before {
+      content: '';
+      position: absolute;
+      width: 500px;
+      height: 775px;
+      top: 0;
+      left: -75px;
+      border-radius: 0 0 400px 400px;
+      background: linear-gradient(var(--clr), var(--clr-two));
+      z-index: -1;
+    }
+
+    &::after {
+      content: '';
+      width: 450px;
+      height: 800px;
+      bottom: 0;
+      right: 0;
+      position: absolute;
+      border-radius: 400px 400px 0 0;
+      opacity: 0.03;
+      background: var(--clr);
+      z-index: -1;
+    }
+  }
+
+  .hero {
+    width: 900px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    place-content: center;
+    &-text {
+      width: 50ch;
+      justify-self: flex-end;
+      align-self: center;
+      font-size: 1.05rem;
+    }
+  }
+
+  h1 {
+    font-size: 2.75rem;
+    font-weight: 600;
   }
 
   //< end me
--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -2,11 +2,149 @@
   console.log('lets make a chat scheduling app?')
 </script>
 
-<div class="phone" />
+<div class="phone-wrap">
+  <div class="phone">
+    <div class="chat-header">
+      <div class="user">
+        <b>-</b>
+
+        <div class="user-avatar">
+          <img src="https://picsum.photos/50/" alt="avatar" />
+        </div>
+
+        <div class="user-title">
+          <h2>Some person</h2>
+          <h3>their title</h3>
+        </div>
+        <ul class="menu">
+          <li />
+          <li />
+          <li />
+        </ul>
+      </div>
+    </div>
+
+    <div class="chat-body">
+      <div class="chat-bubble">This will be a msg</div>
+      <div class="chat-bubble">This will be a msg</div>
+      <div class="chat-bubble">This will be a msg</div>
+      <div class="chat-bubble">This will be a msg</div>
+      <div class="chat-input">
+        <input type="text" placeholder="type a message" />
+        <button>c</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <style type="text/scss">
+  .phone-wrap {
+    width: min-content;
+    padding: 10px;
+    background-color: var(--clr-white);
+    border-radius: 25px;
+  }
+
   .phone {
-    max-width: 340px;
-    height: 560px;
+    width: 250px;
+    height: 500px;
+    border-radius: 20px;
+    background-color: var(--clr-gray);
+  }
+
+  .chat-header {
+    position: relative;
+    width: 100%;
+    color: var(--clr-white);
+    border-radius: 20px 20px 5px 5px;
+    background: linear-gradient(-90deg, var(--clr), var(--clr-two));
+    &::before {
+      position: relative;
+      content: '';
+      width: 125px;
+      height: 20px;
+      margin: 0 auto;
+      display: block;
+      border-radius: 0 0 10px 10px;
+      background-color: var(--clr-white);
+    }
+  }
+
+  .chat-body {
+    padding: 0 0.5rem;
+  }
+
+  .chat-input {
+    height: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: var(--clr-white);
+    border-radius: 20px;
+
+    input {
+      margin-left: 10px;
+      border: none;
+    }
+
+    button {
+      width: 25px;
+      height: 25px;
+      margin-right: 10px;
+      color: var(--clr-white);
+      background-color: var(--clr-black);
+      border: none;
+      border-radius: 50%;
+    }
+  }
+
+  .user {
+    padding: 10px 15px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 6fr 1fr;
+    gap: 0.5rem;
+    align-items: center;
+
+    b {
+      justify-self: flex-end;
+    }
+
+    &-avatar {
+      width: 30px;
+      height: 30px;
+      background-color: var(--clr-black);
+      border: 2px solid var(--clr-white);
+      border-radius: 50%;
+      overflow: hidden;
+    }
+
+    &-title {
+      justify-self: flex-start;
+
+      h2 {
+        margin: 0;
+        font-size: 1.1rem;
+      }
+
+      h3 {
+        margin: 0;
+        color: var(--clr-gray);
+        font-size: 0.7rem;
+        font-weight: 400;
+      }
+    }
+  }
+
+  .menu {
+    width: 5px;
+    display: grid;
+    gap: 2px;
+    justify-self: flex-end;
+
+    li {
+      width: 2px;
+      height: 2px;
+      background-color: var(--clr-white);
+    }
   }
 </style>
